03f08ac1d1f0ac4c01106a22e451645aa32f9d4c
jei3m <virozzegt@gmail.com>
2025-04-26 22:44:58 +0800

    feat: add initial server setup with Express and product routes

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+import productRoutes from  './routes/product.route.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json()); // To allow accept json data in the req.body
+
+app.get("/", (req,res) => {
+    res.send("Hello World!");
+});
+
+app.use("/api/products", productRoutes)
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server is running on port ${PORT}`);
+});
