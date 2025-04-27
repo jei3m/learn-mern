6f8f27a4ce7b0c3666dc804a7a8f15b492ea592d
jei3m <virozzegt@gmail.com>
2025-04-27 17:24:49 +0800

    refactor: add __dirname and __filename for ES module compatibility

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { connectDB } from './config/db.js';
 import productRoutes from  './routes/product.route.js';
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
