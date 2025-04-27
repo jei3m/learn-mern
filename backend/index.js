73c1472d7b84ccf0b3615aa0ef657641fcfe5e3b
jei3m <virozzegt@gmail.com>
2025-04-27 21:43:39 +0800

    cleanup: removed unused lines of code

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,6 @@ import cors from 'cors';
 
 dotenv.config();
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -21,14 +18,6 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes)
-
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(__dirname, '../frontend/dist')));
-
-//     app.get("*", (req, res) => {
-//         res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
-//     });
-// }
 
 app.listen(PORT, () => {
     connectDB();
