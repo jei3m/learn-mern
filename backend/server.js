a5d002ce56cacf35ad07359727df28e5331fc64c
jei3m <virozzegt@gmail.com>
2025-04-27 18:06:58 +0800

    ref: implemented cors

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,17 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { connectDB } from './config/db.js';
 import productRoutes from  './routes/product.route.js';
+import cors from 'cors';
 
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors({ origin: process.env.ORIGIN_URL })); // Allow requests from this origin
 
 app.use(express.json()); // To allow accept json data in the req.body
 
