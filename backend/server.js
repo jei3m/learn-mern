b042419afa8994b702dc6e136af1904ae4747fbe
jei3m <virozzegt@gmail.com>
2025-04-27 17:32:37 +0800

    ref: commented frontend static serving logic

--- backend/server.js
+++ backend/server.js
@@ -17,13 +17,13 @@ app.use(express.json()); // To allow accept json data in the req.body
 
 app.use("/api/products", productRoutes)
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
-    });
-}
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
+//     });
+// }
 
 app.listen(PORT, () => {
     connectDB();
