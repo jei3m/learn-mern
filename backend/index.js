308e7636d2e7a51e3fb7708c806f6160431d1dd2
jei3m <virozzegt@gmail.com>
2025-04-28 00:45:44 +0800

    feat: implemented static file routing for production

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,13 @@ import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import productRoutes from  './routes/product.route.js';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,11 +18,18 @@ app.use(cors({ origin: process.env.ORIGIN_URL })); // Allow requests from this o
 
 app.use(express.json()); // To allow accept json data in the req.body
 
-app.get("/", (req, res) => {
-	res.send("API is running");
-});
+// app.get("/", (req, res) => {
+// 	res.send("API is running");
+// });
 
 app.use("/api/products", productRoutes)
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+    });
+}
 
 app.listen(PORT, () => {
     connectDB();
