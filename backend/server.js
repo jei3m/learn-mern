2ea538e6a78d99073dd4819e8a2f24d9f4d8eddc
jei3m <virozzegt@gmail.com>
2025-04-27 15:04:04 +0800

    refactor: add production environment support and default port fallback

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import { connectDB } from './config/db.js';
 import productRoutes from  './routes/product.route.js';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // To allow accept json data in the req.body
 
@@ -15,6 +16,14 @@ app.get("/", (req,res) => {
 });
 
 app.use("/api/products", productRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname,'frontend', 'dist', 'index.html'));
+    });
+}
 
 app.listen(PORT, () => {
     connectDB();
